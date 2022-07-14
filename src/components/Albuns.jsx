ade7be90364f59fbbb648e58915853b1965e1001
walkerNegrão <walker.esteves.negrao@gmail.com>
2022-07-13 21:31:52 -0300

    Consumo e exibição dos dados na página

--- src/components/Albuns.jsx
+++ src/components/Albuns.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import './Albuns.css'
 
 const Albuns = () => {
-  return (
-    <div>
-        
-    </div>
-  )
+
+    const [albuns, setAlbuns] = useState([])
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/albums').
+            then(response => response.json()).
+            then(data => setAlbuns(data))
+    }, [])
+
+    return (
+        <div>
+            <h3>Dados consumidos:</h3>
+            {albuns.map(album => {
+                return (
+                    <div>
+                        <h4>id:{album.id} - {album.title} </h4>
+                    </div>
+                )
+            })}
+        </div>
+    )
 }
 
 export default Albuns
